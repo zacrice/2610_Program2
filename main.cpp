5d0f3ba556b35e2218708d360e379e8560fca3f7
mummerh <michaelhummer1@gmail.com>
2016-04-24 02:58:17 -0500

    working for the numbers given in the example. Only problem is I am hardcoding the addition to the exponent, so the calculations will be incorrect if there is more than one digit before the decimal point

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@ int main()
 {
     string str1, str2;
     unsigned long long num1, num2, s1, s2, ex1, ex2, frac1, frac2, sign, exponent, fraction, sig1, sig2;
-
     cout<<"Enter a single precision floating point (in hex): ";
     cin>>str1;
     cout<<"Enter another single precision floating point (in hex): ";
@@ -51,6 +50,7 @@ int main()
     frac1 = num1 & 0x00000000007fffff;
     frac2 = num2 & 0x00000000007fffff;
 	
+	//add leading 1 to fraction to create significand e.g. 1.001.....	
 	sig1 = frac1 | 0x0000000000800000;
 	sig2 = frac2 | 0x0000000000800000;
 	
@@ -61,11 +61,38 @@ int main()
     exponent = (ex1 + ex2) - 127;
     //display the output
     cout << "exponent: " << hex << exponent << endl;
-
+	//multiply significands
     fraction = sig1 * sig2;
     cout << "fraction: " << hex << fraction << endl;
-
-
+	//convert fraction to binary string to count leading zeroes
+	string s = bitset<64>(fraction).to_string();
+	char bits[64];
+	strcpy(bits, s.c_str());
+	int zeroes;
+	for (int i = 0; i < 64; i++)
+	{
+		if (bits[i] == '1')
+		{
+			zeroes = i+1; 
+			break;
+		}
+	}
+	//shift left by amount of zeroes to remove first bit
+	fraction = fraction << zeroes;
+	
+	// shift right by 64 - 23 bits to store at correct range
+	fraction = fraction >> (64-23);
+	
+	exponent++; //hard coding exponent change
+	exponent = exponent << 23; //shift exponent to correct range
+	//string ms = bitset<64>(exponent).to_string(); test print exponent
+	//cout << ms << endl;
+	//determine sign
+	sign = s1 xor s2;
+	sign = sign << 31;
+	unsigned long long result = sign | exponent | fraction;
+	cout << "Final Result: " << hex << result <<endl;
+	
 
     return 0;
 }
